3f787ceee21a0979b8bd6bd3a7539e4d84ab498e
mihkeluutar <mihkel.uutar@gmail.com>
2021-04-03 17:12:18 +0300

    test1.2

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ public class Test {
     public static void main(String[] args) {
 
         int test1 = 9;
-
-        System.out.println(test1);
+        int test2 = 1;
+        System.out.println(test1 + " + " + test2 + " = " + (test1 + test2));
     }
 }
